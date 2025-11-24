827648904870806c3526aea80b3cb8fdbb23f0be
Shadab Shaikh <shaikhshadabali2@gmail.com>
2025-11-25 03:06:24 +0530

    feat: binary search implementation

---
+++ 23_binary_search/solutions.py
@@ -0,0 +1,38 @@
+'''
+apply binary search
+
+Example
+
+Input:
+input_list = [1, 2, 3, 4, 5]
+target = 2
+
+Output:
+2, True
+'''
+
+
+def binary_search(input_list, target):
+    left = 0
+    n = len(input_list)
+    right = n - 1
+    while (left <= right):
+        mid = left + (right - left) // 2
+
+        if mid == target:
+            return mid, True
+
+        if mid > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return -1, False
+
+
+print(
+    binary_search(
+        [1, 2, 3, 4, 5],
+        2
+    )
+)
